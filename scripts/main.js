071668f69e7f1fa0b2681eee35facfa3207609d8
ShapovalDenys <denys.igorevich@gmail.com>
2020-04-16 15:21:17 +0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -7,19 +7,19 @@ let active = 0;
 
 const list = document.querySelector('ul');
 const listElems = document.querySelectorAll('li');
-const btn_prev = document.querySelector('.carousel__btn_prev');
-const btn_next = document.querySelector('.carousel__btn_next');
+const btnPrev = document.querySelector('.carousel__btn_prev');
+const btnNext = document.querySelector('.carousel__btn_next');
 const dots = document.querySelectorAll('.carousel__dot');
 
-btn_prev.addEventListener('click', prevAct);
+btnPrev.addEventListener('click', prevAct);
 
 function prevAct() {
   position += width * count;
-  position = Math.min(position, 0)
+  position = Math.min(position, 0);
   list.style.marginLeft = position + 'px';
 }
 
-btn_next.addEventListener('click', nextAct);
+btnNext.addEventListener('click', nextAct);
 
 function nextAct() {
   position -= width * count;
@@ -27,23 +27,26 @@ function nextAct() {
   list.style.marginLeft = position + 'px';
 }
 
-btn_next.addEventListener('click', nextDot);
+btnNext.addEventListener('click', nextDot);
 
-function nextDot () {
+function nextDot() {
   dots[active].classList.remove('carousel__dot_active');
+
   if (active + 1 === dots.length) {
-  dots[active].classList.add('carousel__dot_active')
-  return;
+    dots[active].classList.add('carousel__dot_active');
+
+    return;
   } else {
     active++;
   }
   dots[active].classList.add('carousel__dot_active');
 }
 
-btn_prev.addEventListener('click', prevDot);
+btnPrev.addEventListener('click', prevDot);
 
-function prevDot () {
+function prevDot() {
   dots[active].classList.remove('carousel__dot_active');
+
   if (active === 0) {
     active = 0;
   } else {
@@ -51,4 +54,3 @@ function prevDot () {
   }
   dots[active].classList.add('carousel__dot_active');
 }
-
